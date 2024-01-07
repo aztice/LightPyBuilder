14a48ba1848f362ad423f17862dabaef237109ee
Light <102204002+aztice@users.noreply.github.com>
2024-01-07 14:42:46 +0800

    Create index.php

---
+++ server/index.php
@@ -0,0 +1,8 @@
+<?php
+$codes=$_GET['codes']; // get the source codes
+$mt=mt_rand(0,10000000); // Random Number for saving a python file
+$f='./files/'.$mt.'py';
+file_put_contents($f,$codes); // Save the codes to a python file
+$command = 'start /B cmd /C "pyinstaller '.$f.' --onefile > '.$mt.'.log 2>&1"'; // make commands
+exec($command); // Build
+echo $mt; // return the ID of file
