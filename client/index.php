d03bfb0c8346500170943d99ee8ecbfea450c9a4
Light <102204002+aztice@users.noreply.github.com>
2024-01-07 14:48:33 +0800

    Create index.php

---
+++ client/index.php
@@ -0,0 +1,25 @@
+<!--
+You can edit this page by yourself!
+-->
+<!DOCTYPE html>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <title>LightPyBuilder Project</title>
+</head>
+<body>
+  <h1>LightPyBuilder Project</h1>
+  <p>This page a python build tool:LightPyBuilder</p>
+  <h2>Preview Beta</h2>
+  <blockquote>
+    <table>
+      <form action="submit.php" method="post">
+        <tr>
+          <th><textarea name="data" placeholder="Enter your data!" style="width:400px;height:500px;"></textarea></th>
+          <th><button type="submit" style="font-size:25px;">Build!</button></th>
+        </tr>
+      </table>
+    </form>
+  </blockquote>
+</body>
+</html>
