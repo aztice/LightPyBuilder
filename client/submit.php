8f571ed3718d89d186f18af5054b20bad7422386
Light <102204002+aztice@users.noreply.github.com>
2024-01-07 15:40:56 +0800

    Update submit.php

--- client/submit.php
+++ client/submit.php
@@ -10,5 +10,16 @@ else if(strlen($data)>1000){
     echo "WARNING: TOO MANY DATA,PLEASE REDUCE YOUR DATA!";
     return;
 }
-$id=file_get_contents($api);
+$curl = curl_init();
+
+$data = array('codes' => $data);
+
+curl_setopt($curl, CURLOPT_URL, api());
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+
+$id = curl_exec($curl);
+
+curl_close($curl);
 header('Location: result.php?id='.$id);
